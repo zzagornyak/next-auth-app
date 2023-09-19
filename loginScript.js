6734876c701f726231d92cdfdaffbf3280dfb9ee
Eughene Zahorniak <zzagornyak@gmail.com>
2023-09-19 23:15:02 +0300

    chore: debug env

--- loginScript.js
+++ loginScript.js
@@ -1,6 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+  console.log('VERSEL_URL', process.env.VERCEL_URL);
+  console.log('TEST_LOGIN_PATH', process.env.TEST_LOGIN_PATH);
+  console.log('TEST_USER', process.env.TEST_USER);
+  console.log('TEST_PASSWORD', process.env.TEST_PASSWORD);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(`${process.env.VERCEL_URL}${process.env.TEST_LOGIN_PATH}`);
