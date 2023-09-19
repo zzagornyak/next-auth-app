ad4cc900a3fe60146f657ca3699f5d229d3c5ba1
Eughene Zahorniak <zzagornyak@gmail.com>
2023-09-19 23:20:23 +0300

    feat: add env

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,8 +2,8 @@ module.exports = {
   ci: {
     collect: {
       url: [
-        // `https://${process.env.VERCEL_URL}`,
-        'https://next-auth-app-eta.vercel.app'
+        `https://${process.env.VERCEL_URL}`,
+        // 'https://next-auth-app-eta.vercel.app'
       ],
       numberOfRuns: 3,
     },
