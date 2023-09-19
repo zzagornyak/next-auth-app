0f0ea97e78ab15a60ebd7ab179deb94a34301d46
Eughene Zahorniak <zzagornyak@gmail.com>
2023-09-19 20:08:46 +0300

    feat: test env

---
+++ lighthouserc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        `https://${process.env.VERCEL_URL}`,
+        // 'https://next-auth-app-eta.vercel.app'
+      ],
+      numberOfRuns: 3,
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
