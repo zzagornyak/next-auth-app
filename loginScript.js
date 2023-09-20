7d26e60e8a75fcb49c28b5d24587199a8ede6c43
Eughene Zahorniak <zzagornyak@gmail.com>
2023-09-20 10:19:20 +0300

    chore: debug

--- loginScript.js
+++ loginScript.js
@@ -1,19 +1,38 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  console.log('DEPLOYMENT_URL', process.env.DEPLOYMENT_URL);
-  console.log('TEST_LOGIN_PATH', process.env.TEST_LOGIN_PATH);
-  console.log('TEST_USER', process.env.TEST_USER);
-  console.log('TEST_PASSWORD', process.env.TEST_PASSWORD);
-  const browser = await puppeteer.launch();
+  console.log("Launching browser...");
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(`${process.env.DEPLOYMENT_URL}`);
-
+  
+  console.log("Navigating to login page...");
+  await page.goto('http://localhost:3000/api/auth/signin');
+  
+  console.log("Waiting for username field...");
   await page.waitForSelector('#input-username-for-credentials-provider');
-  await page.type('#input-username-for-credentials-provider', process.env.TEST_USER);
-  await page.type('#input-password-for-credentials-provider', process.env.TEST_PASSWORD);
+  
+  await page.type('#input-username-for-credentials-provider', 'django');
+  
+  const username = await page.$eval('#input-username-for-credentials-provider', el => el.value);
+  console.log("Username typed:", username);
+
+  await page.type('#input-password-for-credentials-provider', 'password');
+
+  const password = await page.$eval('#input-password-for-credentials-provider', el => el.value);
+  console.log("Password typed:", password);
+  
+  console.log("Clicking submit button...");
   await page.click('button[type="submit"]');
 
-  await page.waitForNavigation();
+  console.log("Navigating to middleware page...");
+  await page.goto('http://localhost:3000/middleware');
+  // find h1 tag log its text
+  const h1Text = await page.$eval('h1', el => el.textContent);
+  console.log("H1 text:", h1Text);
+
+
+
+  
+  console.log("Closing browser...");
   await browser.close();
 })();
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,13 +1,12 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import GitHubProvider from "next-auth/providers/github";
 
 export const options: NextAuthOptions = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
+    // GitHubProvider({
+    //   clientId: process.env.GITHUB_CLIENT_ID as string,
+    //   clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    // }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
