c7ca53c9bd14e272ef4f3ca1db3de645de917090
Eughene Zahorniak <zzagornyak@gmail.com>
2023-09-20 22:25:02 +0300

    chore: debug

--- lighthouserc.js
+++ lighthouserc.js
@@ -17,10 +17,10 @@ module.exports = {
     },
     assert: {
       "assertions": {
-          "categories:performance": ["error", {"minScore": 1}],
-          "categories:accessibility": ["error", {"minScore": 1}],
-          "categories:best-practices": ["error", {"minScore": 1}],
-          "categories:seo": ["error", {"minScore": 1}]
+          "categories:performance": ["error", {"minScore": 0.7}],
+          "categories:accessibility": ["error", {"minScore": 0.7}],
+          "categories:best-practices": ["error", {"minScore": 0.7}],
+          "categories:seo": ["error", {"minScore": 0.7}]
       }
     },
   },
