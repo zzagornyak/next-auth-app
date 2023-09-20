ea3221425b19c47c9adea87fed5ac67a6fe7c254
Eughene Zahorniak <zzagornyak@gmail.com>
2023-09-20 21:00:24 +0300

    chore: debug

--- loginScript.js
+++ loginScript.js
@@ -27,15 +27,14 @@ const puppeteer = require('puppeteer');
   console.log("Password typed:", password);
   
   console.log("Clicking submit button...");
-await page.click('button[type="submit"]');
-await page.waitForNavigation();
+  await page.click('button[type="submit"]');
+  await page.waitForNavigation();
 
-console.log("Getting cookies...");
-const cookies = await page.cookies();
-process.env.COOKIES = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
+  console.log("Getting cookies...");
+  const cookies = await page.cookies();
+  cookies.forEach(cookie => {
+    console.log(cookie.name, cookie.value);
+  });
 
-console.log("Cookies obtained:", process.env.COOKIES);
 
-console.log("Closing browser...");
-await browser.close();
 })();
